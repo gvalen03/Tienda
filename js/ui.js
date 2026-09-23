11b350b035c03f4f7c85167ccd509e4fa8b1f3a1
Gloria Idrobo <gloriaidrobo@unicauca.edu.co>
2026-09-22 23:32:26 -0500

    feat: mejorar interfaz y visualización del carrito

--- js/ui.js
+++ js/ui.js
@@ -64,50 +64,35 @@ function renderizarProductos(lista) {
 ========================= */
 
 function renderizarCarrito() {
-
     contenedorCarrito.innerHTML = "";
 
-
     if (carrito.length === 0) {
-
-        contenedorCarrito.innerHTML =
-            "<p>Tu carrito está vacío.</p>";
-
-        totalCarrito.textContent = "$0";
-
-        return;
-    }
-
-
-    carrito.forEach(item => {
-
-        const elemento =
-            document.createElement("div");
-
-        elemento.classList.add("item-carrito");
-
-        const subtotal =
-            item.precio * item.cantidad;
-
-        elemento.innerHTML = `
-            <h3>${item.nombre}</h3>
-
-            <p>
-                Cantidad: ${item.cantidad}
-            </p>
-
-            <p>
-                Subtotal:
-                $${subtotal.toLocaleString("es-CO")}
+        contenedorCarrito.innerHTML = `
+            <p class="carrito__vacio">
+                Tu carrito está vacío.
             </p>
         `;
 
+        totalCarrito.textContent = "$0";
+        return;
+    }
+
+    carrito.forEach(item => {
+        const elemento = document.createElement("div");
+
+        elemento.classList.add("item-carrito");
+
+        const subtotal = item.precio * item.cantidad;
+
+        elemento.innerHTML = `
+            <h3>${item.nombre}</h3>
+            <p>Cantidad: ${item.cantidad}</p>
+            <p>Subtotal: $${subtotal.toLocaleString("es-CO")}</p>
+        `;
+
         contenedorCarrito.appendChild(elemento);
-
     });
-
 
     totalCarrito.textContent =
         `$${calcularTotal().toLocaleString("es-CO")}`;
-
 }
